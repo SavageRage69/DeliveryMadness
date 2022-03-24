dc5bc13ce69f354c9f297e104e1f6b9b0c10d87d
Nicholas E.R.D <83941963+SavageRage69@users.noreply.github.com>
2022-03-24 07:20:50 +0100

    Update Timer.lua

--- Timer.lua
+++ Timer.lua
@@ -1,23 +1,23 @@
 --timer
 
 function update_timer()
-		game_timer=time()-game_timer_start
+    game_timer=time()-game_timer_start
 end
 
 function draw_timer()
-		if crt==true then
-		fillp(▤\1|0b.011)
-		else
-		fillp()
-		end
+    if crt==true then
+	    fillp(▤\1|0b.011)
+	else
+	    fillp()
+	end
 		
-		if game_timer<=9.99 then
+	if game_timer<=9.99 then
 		rectfill(cam_x,cam_y+121,cam_x+15,cam_y+128,0)
-		elseif game_timer<=99.99 then
+	elseif game_timer<=99.99 then
 		rectfill(cam_x,cam_y+121,cam_x+19,cam_y+128,0)
-		elseif game_timer<=999.99 then
+	elseif game_timer<=999.99 then
 		rectfill(cam_x,cam_y+121,cam_x+24,cam_y+128,0)
-		end
+	end
 		
-		print(flr(game_timer * 100) / 100, cam_x, cam_y+122,7)
+	print(flr(game_timer * 100) / 100, cam_x, cam_y+122,7)
 end
